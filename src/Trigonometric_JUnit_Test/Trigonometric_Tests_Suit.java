462f197570bd998873d5a5e9880a50a7013a3b80
kavisha14 <solankikavisha24@gmail.com>
2020-11-08 14:36:29 +0530

    Added Suite.test

---
+++ src/Trigonometric_JUnit_Test/Trigonometric_Tests_Suit.java
@@ -0,0 +1,12 @@
+package Trigonometric_JUnit_Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+ 
+@RunWith(Suite.class)
+@SuiteClasses({Tests_Sin.class,Tests_Cos.class,Tests_Tan.class,Tests_factTesting.class,Tests_powerTesting.class})
+
+public class Trigonometric_Tests_Suit {
+	
+}
