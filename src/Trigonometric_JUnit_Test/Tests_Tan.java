8901d9332d72f3015c54a1bac99de45ed255e43a
kavisha14 <solankikavisha24@gmail.com>
2020-11-11 12:40:20 +0530

    Resolved errors and added more test cases in Tests_Tan.java

--- src/Trigonometric_JUnit_Test/Tests_Tan.java
+++ src/Trigonometric_JUnit_Test/Tests_Tan.java
@@ -1,7 +1,7 @@
 package Trigonometric_JUnit_Test;
 
 import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import Trigonometric.Factorial_Exception_Handler;
@@ -16,29 +16,151 @@ public class Tests_Tan {
 	public void testTan1_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
 	{
 		// this test is used for checking tan(x) value compare to Math.tan(x)
-		assertEquals(Math.tan(2), Trigonometric_Functions.tan(2), decimalpoints);
+		Assert.assertEquals(Math.tan(2), Trigonometric_Functions.tan(2), decimalpoints);
 	}
-
+	
 	@Test
 	public void testTan1_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
 	{
 		// this test is used for finding that the function will generate accurate value
 				// or not
-		assertEquals(Math.tan(20), Trigonometric_Functions.tan(2), decimalpoints);
+		Assert.assertNotEquals(Math.tan(20), Trigonometric_Functions.tan(2), decimalpoints);
 	}
 	
 	@Test
 	public void testTan2_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
 	{
-		// this test is used for finding value for Degree
-		assertEquals(1.7320, Trigonometric_Functions.tan(Trigonometric_Functions.degree_To_Radian(60)), decimalpoints);
+		// this test is used for checking tan(x) value compare to Math.tan(x)
+		Assert.assertEquals(Math.tan(29), Trigonometric_Functions.tan(29), decimalpoints);
 	}
 	
-		@Test
+	@Test
 	public void testTan2_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
 	{
-		// this test is used for finding value for Degree
-		assertEquals(1.7320, Trigonometric_Functions.tan(Trigonometric_Functions.degree_To_Radian(6)), decimalpoints);
+		// this test is used for finding that the function will generate accurate value
+				// or not
+		Assert.assertNotEquals(Math.tan(20), Trigonometric_Functions.tan(30), decimalpoints);
 	}
 	
+	@Test
+	public void testTan3_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for checking tan(x) value compare to Math.tan(x)
+		Assert.assertEquals(Math.tan(45), Trigonometric_Functions.tan(45), decimalpoints);
+	}
+	
+	@Test
+	public void testTan3_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding that the function will generate accurate value
+				// or not
+		Assert.assertNotEquals(Math.tan(20), Trigonometric_Functions.tan(45), decimalpoints);
+	}
+	
+	@Test
+	public void testTan4_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for checking tan(x) value compare to Math.tan(x)
+		Assert.assertEquals(Math.tan(66), Trigonometric_Functions.tan(66), decimalpoints);
+	}
+	
+	@Test
+	public void testTan4_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding that the function will generate accurate value
+				// or not
+		Assert.assertNotEquals(Math.tan(20), Trigonometric_Functions.tan(66), decimalpoints);
+	}
+	
+	@Test
+	public void testTan5_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for checking tan(x) value compare to Math.tan(x)
+		Assert.assertEquals(Math.tan(89), Trigonometric_Functions.tan(89), decimalpoints);
+	}
+	
+	@Test
+	public void testTan5_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding that the function will generate accurate value
+				// or not
+		Assert.assertNotEquals(Math.tan(20), Trigonometric_Functions.tan(89), decimalpoints);
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testTan6_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(0)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(0)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan6_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertNotEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(6)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(0)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan7_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(30)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(30)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan7_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertNotEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(3)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(30)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan8_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(45)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(45)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan8_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertNotEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(4)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(45)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan9_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(60)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(60)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan9_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertNotEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(6)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(60)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan10_Positive() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(89)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(89)), decimalpoints);
+	}
+	
+	@Test
+	public void testTan10_Negative() throws Power_Exception_Handler, Factorial_Exception_Handler
+	{
+		// this test is used for finding value for Degree
+		Assert.assertNotEquals(Math.tan(Trigonometric_Functions.Degree_To_Radian(9)), Trigonometric_Functions.tan(Trigonometric_Functions.Degree_To_Radian(90)), decimalpoints);
+	}
+	
+	
+
 }
